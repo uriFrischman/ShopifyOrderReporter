de40c1fedf3ed4589a81ed83a8eb28e1acd0e246
Uri Frischman <uri.frischman@gmail.com>
2017-09-14 05:39:01 -0400

    initializing main page variables and setting up the tickers

--- app/src/main/java/com/frischman/uri/shopifyorderreporter/MainActivity.java
+++ app/src/main/java/com/frischman/uri/shopifyorderreporter/MainActivity.java
@@ -5,9 +5,30 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TickerView mNapoleanTicker;
+    private TickerView mBagsTicker;
+    private Button mButtonGenerateReport;
+    private TextView mResetText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButtonGenerateReport = (Button) findViewById(R.id.generateReportButton);
+        mResetText = (TextView) findViewById(R.id.resetText);
+    private void resetTickers() {
+        mBagsTicker.setText("0", true);
+        mNapoleanTicker.setText("$0000.00", true);
+    }
+    private void initTickers() {
+        mNapoleanTicker = (TickerView) findViewById(R.id.napoleanTicker);
+        mNapoleanTicker.setCharacterList(TickerUtils.getDefaultListForUSCurrency());
+        mNapoleanTicker.setAnimationDuration(3000);
+
+        mBagsTicker = (TickerView) findViewById(R.id.bagsAmountTicker);
+        mBagsTicker.setCharacterList(TickerUtils.getDefaultNumberList());
+        mBagsTicker.setAnimationDuration(3000);
+        mBagsTicker.setAnimationInterpolator(new OvershootInterpolator());
+
+        resetTickers();
     }
 }
