2f96c2b3a20a6dfaf6cb5207a0e79e1cfb8ae67b
Uri Frischman <uri.frischman@gmail.com>
2017-09-14 05:47:26 -0400

    imports, spaces and constant name change

--- app/src/main/java/com/frischman/uri/shopifyorderreporter/MainActivity.java
+++ app/src/main/java/com/frischman/uri/shopifyorderreporter/MainActivity.java
@@ -1,21 +1,40 @@
 package com.frischman.uri.shopifyorderreporter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.OvershootInterpolator;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.frischman.uri.shopifyorderreporter.util.StringUtil;
+import com.robinhood.ticker.TickerUtils;
+import com.robinhood.ticker.TickerView;
+
+import static com.frischman.uri.shopifyorderreporter.R.string.list_of_orders_url;
+import static com.frischman.uri.shopifyorderreporter.util.CustomerUtil.getAmountCustomerSpentFromOrders;
+import static com.frischman.uri.shopifyorderreporter.util.LineItemUtil.getTotalLineItemSoldFromOrders;
+import static com.frischman.uri.shopifyorderreporter.util.OrderUtil.getAllOrdersFromRequest;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int NAPOLEAN_LOADER = 1;
-    private final int BAGS_TICKER = 2;
+    private final int BAGS_LOADER = 2;
     private Context mContext = this;
     private TickerView mNapoleanTicker;
     private TickerView mBagsTicker;
     private Button mButtonGenerateReport;
     private TextView mResetText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mButtonGenerateReport = (Button) findViewById(R.id.generateReportButton);
         mResetText = (TextView) findViewById(R.id.resetText);
 
@@ -57,17 +76,19 @@ public class MainActivity extends AppCompatActivity {
 
         mButtonGenerateReport.setOnClickListener(v -> {
             getSupportLoaderManager().initLoader(NAPOLEAN_LOADER, null, amountLoader).forceLoad();
-            getSupportLoaderManager().initLoader(BAGS_TICKER, null, bagsLoader).forceLoad();
+            getSupportLoaderManager().initLoader(BAGS_LOADER, null, bagsLoader).forceLoad();
         });
 
         mResetText.setOnClickListener(v -> {
             resetTickers();
         });
     }
+
     private void resetTickers() {
         mBagsTicker.setText("0", true);
         mNapoleanTicker.setText("$0000.00", true);
     }
+
     private void initTickers() {
         mNapoleanTicker = (TickerView) findViewById(R.id.napoleanTicker);
         mNapoleanTicker.setCharacterList(TickerUtils.getDefaultListForUSCurrency());
